723cd92f584f65fc81d7e2625716b0090a9d5faf
Rahul Yadav <rahuldy143@gmail.com>
2026-07-08 20:14:30 +0530

    routing_testing: created some testing routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,21 +2,56 @@ const express = require('express'); // it will return a createApplication functi
 
 const createApp = express();
 
-// console.log(createApp);
-createApp.use("/rahul",(req, res) => {
-    res.send("Hello from server");
-    console.log("Hey hii");
+// if i use below route every route of /rahul with all method will call this handler only
+// createApp.use("/rahul",(req, res) => {
+//     res.send("This http GET method");
+// });
+
+// handle the file path
+// createApp.get("/rahul/*filepath",(req, res) => {
+//     // requesr url: http://localhost:5450/rahul/new.txt
+//     // response: new.txt
+//     res.send(`File: ${req.params.filepath.join('/')}`);
+// });
+
+// fetching the query
+createApp.get("/rahul/",(req, res) => {
+    console.log(req.query);
+    // requesr url: http://localhost:5450/rahul/?username=R&age=20
+    // response: [Object: null prototype] { username: 'R', age: '20' }
 });
 
-createApp.use("/boss",(req, res) => { // it is route with req & res, if anythig is matched it will execute
-    res.send("Hello from BOSS server");
-    console.log("Hey hii");
+// fetching the params
+createApp.get("/rahul/:userId/:userName",(req, res) => {
+    console.log(req.params);
+    // requesr url: http://localhost:5450/rahul/101/Test
+    // response: [Object: null prototype] { userId: '101', userName: 'Test' }
 });
 
-createApp.use("/",(req, res) => {
-    res.send("This is default route!!");
-    console.log("Hey hii 11");
+createApp.get("/rahul*ya",(req, res) => {
+    console.log("We can add anything between rahul and ya");
+    // requesr url: http://localhost:5450/rahul123456789ya
 });
+
+// createApp.get("/rahul",(req, res) => {
+//     res.send("This http GET method");
+// });
+
+// createApp.post("/rahul",(req, res) => {
+//     res.send("This http POST method");
+// });
+
+// createApp.put("/rahul",(req, res) => {
+//     res.send("This http PUT method");
+// });
+
+// createApp.patch("/rahul",(req, res) => {
+//     res.send("This http PATCH method");
+// });
+
+// createApp.delete("/rahul",(req, res) => {
+//     res.send("This http DELETE method");
+// });
 
 createApp.listen(5450, () =>  {
     console.log("Server is running");
