3bb82347d15d186b4f109e478b8b91b1ce1239bc
rahuldyadav <rahuldy143@gmail.com>
2026-07-13 04:11:31 -0400

    mongoos_conn: added all http method

--- backend/src/database/model/user.js
+++ backend/src/database/model/user.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
     firstName: String,
     lastName: String,
     email: String,
     password: String,
-    age: Number,
-    gender: String
+    age: { type: Number, min: 18, max: 50 },
+    gender: String,
+    createdAt: { type: Date, default: Date.now }
 });
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model("users", userSchema);
 
 module.exports = User;
